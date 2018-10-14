09509bb9fb5ea0dd96c9e0810f0cd5d651a70507
Putti Tangtrakulwongse <puttitang@puttitang-macbookpro2.roam.corp.google.com>
2018-10-14 13:17:07 +0700

    third

--- src/com/putti/web1/Servlet1/Servlet1.java
+++ src/com/putti/web1/Servlet1/Servlet1.java
@@ -27,7 +27,8 @@ public class Servlet1 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("ABC-Sunday2-Committed - Served at: ").append(request.getContextPath());
+		response.getWriter().append("XYZ-Sunday2-Committed - Served at: ").append(request.getContextPath());
+		System.out.println("write something here:" + java.net.InetAddress.getLocalHost().getHostName());
 	}
 
 	/**
